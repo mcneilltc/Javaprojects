67c72846a82413c45e4eb8ce71d99ea411c5db4d
Tiquila McNeill <tiquilamceille@gmail.com>
2019-06-14 15:38:21 -0400

    this loop

--- wheeloffortune/src/Menu.java
+++ wheeloffortune/src/Menu.java
@@ -1,4 +1,7 @@
 public class Menu {
+    ArrayList<Player> players = new ArrayList<Player>();
+    Player currentPlayer = players.get(1);
+System.out.println(“Current player is :” + currentPlayer.getName());
     public static void main(String[] args) {
 
 Players.players();
@@ -6,5 +9,7 @@ Players.choices();
 //Puzzle.RandomPuzzle();
 Spin.spinWheel();
 Vowels.letters();
+
+
     }
 }
