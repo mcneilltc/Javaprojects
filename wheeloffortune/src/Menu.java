86e537e31106e6c70335dfb0c2715687b98ab0b6
Tiquila McNeill <tiquilamceille@gmail.com>
2019-06-14 15:44:54 -0400

    nothing changed

--- wheeloffortune/src/Menu.java
+++ wheeloffortune/src/Menu.java
@@ -2,6 +2,7 @@ public class Menu {
     ArrayList<Player> players = new ArrayList<Player>();
     Player currentPlayer = players.get(1);
 System.out.println(“Current player is :” + currentPlayer.getName());
+
     public static void main(String[] args) {
 
 Players.players();
